6d55be8dfdfbcdf22d717e3bb9de5b3697fd01e6
Pavel Nikolaev <panikolaev7@gmail.com>
2025-02-05 19:36:36 +0300

    Add Bootstrap and pagination

--- apps_django/recipe/urls.py
+++ apps_django/recipe/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from .views import (
     RecipeListView,
     RecipeDetailView,
+    RecipeFromCategory,
 )
 
 urlpatterns = [
     path('', RecipeListView.as_view(), name='home'),
     path('recipe/<slug:slug>/', RecipeDetailView.as_view(), name='recipe_detail'),
+    path('category/<slug:slug>/', RecipeFromCategory.as_view(), name="recipe_by_category"),
 ]
--- apps_django/recipe/views.py
+++ apps_django/recipe/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render
-from .models import Recipe
+from .models import Recipe, Category
 from django.views.generic import ListView, DetailView
+from django.shortcuts import get_object_or_404
+
 
 # Create your views here.
 class RecipeListView(ListView):
     model = Recipe
     template_name = 'recipe/recipe_list.html'
     context_object_name = 'recipes'
+    paginate_by = 5
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Главная страница'
+        context['categories'] = Category.objects.all()
         return context
+
 
 class RecipeDetailView(DetailView):
     model = Recipe
@@ -21,4 +26,28 @@ class RecipeDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = self.object.title
+        context['categories'] = Category.objects.all()
+        return context
+
+
+class RecipeFromCategory(ListView):
+    template_name = 'recipe/recipe_list.html'
+    context_object_name = 'recipes'
+    category = None
+    paginate_by = 5
+
+    def get_queryset(self):
+        """
+        Возвращает рецепты только для текущей категории.
+        """
+        self.category = get_object_or_404(Category, slug=self.kwargs['slug'])
+        return Recipe.objects.filter(category=self.category)
+
+    def get_context_data(self, **kwargs):
+        """
+        Добавляем название категории в контекст.
+        """
+        context = super().get_context_data(**kwargs)
+        context['title'] = f"Категория: {self.category.title}"
+        context['categories'] = Category.objects.all()
         return context
