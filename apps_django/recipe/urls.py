2653f5b338515f7c48fe7c837ecbfd0516e09f1a
Pavel Nikolaev <panikolaev7@gmail.com>
2025-02-05 14:52:47 +0300

    Add view RecipeList

---
+++ apps_django/recipe/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import (
+    RecipeListView,
+)
+
+urlpatterns = [
+    path('', RecipeListView.as_view(), name='home'),
+]
--- apps_django/recipe/views.py
+++ apps_django/recipe/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Recipe
+from django.views.generic import ListView
 
 # Create your views here.
+class RecipeListView(ListView):
+    model = Recipe
+    template_name = 'recipe/recipe_list.html'
+    context_object_name = 'recipes'
